af4d04bf3ca65a78ce8686cf68057d426fd6872d
Daniel Unapanta <dunapanta@outlook.es>
2022-10-27 21:35:39 -0500

    Add CacheFirst

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,3 +1,22 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+
+registerRoute(
+  new RegExp(
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  ),
+  new CacheFirst()
+);
+
+registerRoute(
+    new RegExp(
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+    ),
+    new CacheFirst()
+  );
