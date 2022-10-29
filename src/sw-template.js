2077e452a8639c7620277745df3e7b21d70d84eb
Daniel Unapanta <dunapanta@outlook.es>
2022-10-28 20:07:37 -0500

    Add offline request

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,23 +1,20 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
-
+workbox.loadModule("workbox-background-sync");
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
-  new RegExp(
-    "http://localhost:4000/api/auth/renew"
-  ),
+  new RegExp("http://localhost:4000/api/auth/renew"),
   new NetworkFirst()
 );
 
 registerRoute(
-  new RegExp(
-    "http://localhost:4000/api/events"
-  ),
+  new RegExp("http://localhost:4000/api/events"),
   new NetworkFirst()
 );
 
@@ -29,8 +26,20 @@ registerRoute(
 );
 
 registerRoute(
-    new RegExp(
-      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
-    ),
-    new CacheFirst()
-  );
+  new RegExp(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"
+  ),
+  new CacheFirst()
+);
+
+//Posteos Offline
+const bgSyncPlugin = new BackgroundSyncPlugin("posteo-offline", {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+registerRoute(
+  new RegExp("http://localhost:4000/api/events"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
